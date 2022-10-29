5e0bcd555096f4660aa1ce5e11e5cedfdd65e409
Fauzi <fauzirahmandhani@gmail.com>
2022-10-29 12:37:07 +0700

    Mengubah Catatan

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,30 @@
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+} = require('./handler');
 
-const routes = [
-    {
-      method: 'POST',
-      path: '/notes',
-      handler: addNoteHandler,
-    },
-    {
-      method: 'GET',
-      path: '/notes',
-      handler: getAllNotesHandler,
-    },
-    {
-      method: 'GET',
-      path: '/notes/{id}',
-      handler: getNoteByIdHandler,
-    },
-  ];
+const routes = [{
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+];
 
-  module.exports = routes;
+module.exports = routes;
